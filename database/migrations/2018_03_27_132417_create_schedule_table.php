2606686d1f52e86cc7b6070209b6e51b34401e88
Aditya Suryadi <aditpastrana@gmail.com>
2018-03-27 21:38:46 +0700

    Initial commit

---
+++ database/migrations/2018_03_27_132417_create_schedule_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateScheduleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedule', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('insta_account_id');
+            $table->time('time');
+            $table->enum('status',['active','inactive']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedule');
+    }
+}
